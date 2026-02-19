a67b7e9fafbdfb30173ae7309ba2ca0dceac6b62
Job Portal Deployer <deployer@example.com>
2026-02-19 17:56:16 +0530

    Customize Django admin branding

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,9 @@
-from django.contrib import admin # type: ignore
+from django.contrib import admin
 from .models import Job, Application
+
+admin.site.site_header = "Alexa Administration"
+admin.site.site_title = "Alexa Admin Portal"
+admin.site.index_title = "Welcome to Alexa Admin"
 
 admin.site.register(Job)
 admin.site.register(Application)
