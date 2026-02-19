3d84a78e5d08d222e3add03fa863409123f8ad66
Job Portal Deployer <deployer@example.com>
2026-02-19 17:45:11 +0530

    Auto create superuser on Render

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+python manage.py createsuperuser \
+  --noinput \
+  --username "$DJANGO_SUPERUSER_USERNAME" \
+  --email "$DJANGO_SUPERUSER_EMAIL" || true
